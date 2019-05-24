7f7f967b59fbdef560c0c2e4c4b07e03d8e388bb
Ari Punavuori <ari.punavuori@gmail.com>
2019-05-24 13:05:52 +0300

    Cleaned asset problems

---
+++ GameDesign_MurderMystery/Assets/Murder_Mystery/Scripts/GarageDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageDoor : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip audioclip;
+
+    private void OnTriggerEnter(Collider other) {
+        //if(other.keys == true) {
+        //    open door
+        //}
+        audioSource.PlayOneShot(audioclip);
+    }
+}
