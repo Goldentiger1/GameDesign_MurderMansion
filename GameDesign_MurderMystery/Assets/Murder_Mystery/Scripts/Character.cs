22d8d13a0c1a0743ca219ee6e9ad14deffce0249
Ari Punavuori <ari.punavuori@gmail.com>
2019-05-24 14:00:00 +0300

    Dude + KitchenScripts

---
+++ GameDesign_MurderMystery/Assets/Murder_Mystery/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public bool keysFound;
+    public bool beenToKitchen;
+
+    public void FindKeys() {
+        keysFound = true;
+    }
+
+    public bool PlayerHasFoundKeys() {
+        return keysFound;
+    }
+
+    public void PlayerGoesToKitchen() {
+        beenToKitchen = true;
+    }
+
+    public bool PlayerHasBeenToKitchen() {
+        return beenToKitchen;
+    }
+}
--- GameDesign_MurderMystery/Assets/Murder_Mystery/Scripts/GarageDoor.cs
+++ GameDesign_MurderMystery/Assets/Murder_Mystery/Scripts/GarageDoor.cs
@@ -6,11 +6,21 @@ public class GarageDoor : MonoBehaviour {
 
     public AudioSource audioSource;
     public AudioClip audioclip;
+    bool doorOpened;
 
     private void OnTriggerEnter(Collider other) {
-        //if(other.keys == true) {
-        //    open door
-        //}
-        audioSource.PlayOneShot(audioclip);
+        var character = other.gameObject.GetComponent<Character>();
+        if(character != null) {
+            if(character.PlayerHasFoundKeys()) {
+                // Open Door
+                print("Open Door!");
+                if(!doorOpened) {
+                    doorOpened = true;
+                    transform.LookAt(Vector3.right, Vector3.up);
+                }
+            } else {
+                audioSource.PlayOneShot(audioclip);
+            }
+        }
     }
 }
