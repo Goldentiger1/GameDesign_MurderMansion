efb56faf6de2f3c5ef3a2dd6c3c44e7aff6222e0
Goldentiger_1 <aaro.leskinen@kolumbus.fi>
2019-05-27 01:09:14 +0300

    Koodia ekaan huoneeseen. yritän saada koululla valmiiksi aikaisin.

--- GameDesign_MurderMystery/Assets/playerController.cs
+++ GameDesign_MurderMystery/Assets/playerController.cs
@@ -6,15 +6,27 @@ public class playerController : MonoBehaviour
 {
     public GameObject player;
     public Animation anim;
-    public GameObject cutsceneTrigger;
+    public bool hasKnife = false;
+    public bool hasMirror = false;
+    public bool hasBalcony = false;
     void Start()
     {
-        player = GameObject.Find("FPSController").gameObject;
+       // player = 
         //anim = 
     }
 
-    void onTriggerEnter()
+    void onTriggerEnter(Collider csTriggers)
     {
-
+        if(csTriggers.gameObject.tag == "Balcony")
+        {
+            hasBalcony = true;
+        } else if (csTriggers.gameObject.tag == "Mirror" && hasBalcony == false && hasMirror == false && hasKnife == true)
+        {
+            hasMirror = true;
+        }else if(csTriggers.gameObject.tag == "Knife" && hasBalcony == false && hasMirror == false && hasKnife == false)
+        {
+            print("inside collider");
+            hasKnife = true;
+        }
     }
 }
